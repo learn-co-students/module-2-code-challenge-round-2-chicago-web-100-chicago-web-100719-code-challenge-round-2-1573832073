9e592d3b295d9339e89b19701e3ec02b2290197e
github NikolaCzv <github nikola.raicic@hotmail.com>
2019-11-15 10:33:48 -0600

    all done

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -3,4 +3,14 @@ class EpisodesController < ApplicationController
   def index
     @episodes = Episode.all
   end
+
+  def show
+    find_episode
+  end
+
+  private
+
+  def find_episode
+    @episode = Episode.find(params[:id])
+  end
 end
--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -3,4 +3,14 @@ class GuestsController < ApplicationController
   def index
     @guests = Guest.all
   end
+
+  def show
+    find_guest
+  end
+
+  private
+
+  def find_guest
+    @guest = Guest.find(params[:id])
+  end
 end
